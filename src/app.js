9647134f656f995c7982101623a0b73d0afe8549
DMend024 <68937950+DMend024@users.noreply.github.com>
2020-10-19 22:30:10 -0700

    City search weather api added.

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ return `${day} ${hour}:${minutes}`;
 
 function localTemp(respond) {
   let coordsTemp = Math.round(respond.data.main.temp);
-  console.log(respond);
+  
   let windSpeed=document.querySelector("#wind-speed");
   let wind = (respond.data.wind.speed);
   let humidity=document.querySelector("#humidity");
@@ -54,38 +54,40 @@ function celsiusConvert(event) {
   fahrenheitLink.classList.remove("active");
   celsiusLink.classList.add("active");
   let temperature = document.querySelector("#temp-show");
-  temperature.innerHTML = `HI 42°C`;
+  temperature.innerHTML = `HI ${cityTemp} °C`;
 }
 function fahrenheitConvert(event) {
   event.preventDefault();
+
   celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
   let temperature = document.querySelector("#temp-show");
-  temperature.innerHTML = "HI 108 °F";
+  temperature.innerHTML = `HI ${cityTemp} °F`;
 
 }
 
 function showCity(event) {
-  event.preventDefault();
+ event.preventDefault();
   let cityName = document.querySelector("#home-city");
   let newCity = document.querySelector("#city-input");
   cityName.innerHTML = `${newCity.value}`;
+  citySearch(newCity.value);
 }
 function citySearchTemp(response) {
-  let cityTemp = Math.round(response.data.main.temp);
+    let cityTemp = Math.round(response.data.main.temp);
   let cityTempElement = document.querySelector("#temp-show");
   cityTempElement.innerHTML = `${cityTemp}°F`;
-
 }
 
 function citySearch(city) {
-console.log(city);
   let units = "imperial";
   let apiKey="88859cfce5e0480cf3a55a6721b52598";
-  let apiSearch ="https://api.openweathermap.org/data/2.5/weather";
-  let apiSearchUrl = `${apiSearch}?q=${showCity}&appid=${apiKey}&units=${units}`;
-  axios.get(apiSearchUrl).then(citySearchTemp);
+  let apiSearch = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiSearch}?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(citySearchTemp);
 }
+
+
 navigator.geolocation.getCurrentPosition(findCoords);
 let button = document.querySelector("button");
 button.addEventListener("click", localTemp);
