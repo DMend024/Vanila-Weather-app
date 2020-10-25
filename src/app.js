c1b9c3d34a307c7f56d5190a8fa1fc159c167839
DMend024 <68937950+DMend024@users.noreply.github.com>
2020-10-25 15:32:30 -0700

    Unit conversion set.

--- src/app.js
+++ src/app.js
@@ -22,24 +22,20 @@ function dateToday(date) {
 return `${day} ${hour}:${minutes}`;
 }
 
-
 function displayLocalTemp(respond) {
-    console.log(respond);
-  let showCurrentTemp = Math.round(respond.data.main.temp);
+  celsiusTemperature = Math.round(respond.data.main.temp);
   let windSpeed=document.querySelector("#wind-speed");
   let wind = (respond.data.wind.speed);
   let humidity=document.querySelector("#humidity");
   let humid=(respond.data.main.humidity);
   let currentTemp = document.querySelector("#temp-show");
-  let high=document.querySelector("#high-temp");
-  let highTemp=Math.round(respond.data.main.temp_max); 
-  let low=document.querySelector("#low-temp");
-  let lowTemp= Math.round(respond.data.main.temp_min);
   let icon =(respond.data.weather[0].icon);
   let iconImage=document.querySelector("#temp-icon");
   let cityTempDescriptor=document.querySelector("#weather-description");
-  let description= (respond.data.weather[0].main)   
-  currentTemp.innerHTML = `${showCurrentTemp}°C`; 
+  let description= (respond.data.weather[0].main);   
+  highTemp=Math.round(respond.data.main.temp_max); 
+  lowTemp= Math.round(respond.data.main.temp_min);
+  currentTemp.innerHTML = `${celsiusTemperature}°C`; 
   windSpeed.innerHTML= `Windspeed: ${wind} km/h`;
   humidity.innerHTML=  `Humidity: ${humid}%`;
   high.innerHTML=`HI ${highTemp}°C`;
@@ -60,48 +56,20 @@ function findCoords(position) {
   axios.get(apiUrl).then(displayLocalTemp);
 }
 
-function celsiusConvert(event) {
-  event.preventDefault()
-
-  fahrenheitLink.classList.remove("active");
-  celsiusLink.classList.add("active");
-  let celsiusTemperature = document.querySelector("#temp-show");
-  celsiusTemperature.innerHTML= `°C`
-}
-function fahrenheitConvert(event) {     
-  event.preventDefault();
-
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-
-  let temperature = document.querySelector("#temp-show");
-  temperature.innerHTML =  `°F`;
-
-}
-
-function showCity(event) {
- event.preventDefault();
-  let cityName = document.querySelector("#home-city");
-  let newCity = document.querySelector("#city-input");
-  cityName.innerHTML = `${newCity.value}`;
-  citySearch(newCity.value);
-}
 function displayCityTemp(respond){
-  let showCityTemp = Math.round(respond.data.main.temp);
+  celsiusTemperature = Math.round(respond.data.main.temp);
   let windSpeed=document.querySelector("#wind-speed");
   let wind = (respond.data.wind.speed);
   let humidity=document.querySelector("#humidity");
   let humid=(respond.data.main.humidity);
-  let showCity = document.querySelector("#temp-show");
-  let high=document.querySelector("#high-temp");
-  let highTemp=Math.round(respond.data.main.temp_max);
-  let low=document.querySelector("#low-temp");
-  let lowTemp= Math.round(respond.data.main.temp_min);
+  let showCity = document.querySelector("#temp-show"); 
   let iconImage=document.querySelector("#temp-icon");
   let icon =(respond.data.weather[0].icon);
   let cityTempDescriptor=document.querySelector("#weather-description");
-  let description= (respond.data.weather[0].main)   
-  showCity.innerHTML = `${showCityTemp}°C`; 
+  let description= (respond.data.weather[0].main);
+  highTemp=Math.round(respond.data.main.temp_max);
+  lowTemp= Math.round(respond.data.main.temp_min);
+  showCity.innerHTML = `${celsiusTemperature}°C`; 
   windSpeed.innerHTML= `Windspeed: ${wind} km/h`;
   humidity.innerHTML=  `Humidity: ${humid}%`;
   high.innerHTML=`HI ${highTemp}°C`;
@@ -117,6 +85,50 @@ function citySearch(city) {
   let apiUrl = `${apiSearch}?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayCityTemp);
 }
+function showCity(event) {
+ event.preventDefault();
+  let cityName = document.querySelector("#home-city");
+  let newCity = document.querySelector("#city-input");
+  cityName.innerHTML = `${newCity.value}`;
+  citySearch(newCity.value);
+}
+
+
+function celsiusConvert(event) {
+  event.preventDefault()
+
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  let temperature=document.querySelector("#temp-show");
+  temperature.innerHTML = `${celsiusTemperature}°C`;
+  high.innerHTML=`HI ${highTemp}°C`;
+  low.innerHTML=`LO ${lowTemp}°C`;
+
+}
+
+function fahrenheitConvert(event) {     
+  event.preventDefault();
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+let temperature=document.querySelector("#temp-show");
+  let fahrTemperature = Math.round((celsiusTemperature*9)/5+32);
+  let fahrHighTemp=Math.round((highTemp*9)/5+32);
+  let fahrLowTemp=Math.round((lowTemp*9)/5+32);
+  temperature.innerHTML = `${fahrTemperature}°F`;
+  high.innerHTML=`HI ${fahrHighTemp}°F`;
+  low.innerHTML=`LO ${fahrLowTemp}°F`;
+  
+
+}
+
+
+let celsiusTemperature=null;
+let highTemp=null;
+let lowTemp=null;
+let high=document.querySelector("#high-temp");
+let low=document.querySelector("#low-temp");
+
 
 navigator.geolocation.getCurrentPosition(findCoords);
 let button = document.querySelector("#current-button");
